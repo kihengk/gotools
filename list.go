7b59d7bee68bb23c9a5c0688586e08e7cd89ee2f
kwonkiheng <kihengk@gmail.com>
2022-10-10 02:07:40 +0900

    list

---
+++ list.go
@@ -0,0 +1,48 @@
+package tools
+
+type List[T comparable] struct {
+	l []T
+}
+
+func (l *List[T]) Push(v T) {
+	l.l = append(l.l, v)
+}
+
+func (l *List[T]) Insert(v T) {
+	l.InsertAt(0, v)
+}
+
+func (l *List[T]) InsertAt(pos int, v T) {
+	l.l = append(l.l[:pos+1], l.l[pos:]...)
+	l.l[0] = v
+}
+
+func (l *List[T]) Remove(i int) {
+	l.l = l.l[:i+copy(l.l[i:], l.l[i+1:])]
+}
+
+func (l *List[T]) Equals(rhs *List[T]) bool {
+	if len(l.l) != len(rhs.l) {
+		return false
+	}
+	for i := 0; i < len(l.l); i++ {
+		if l.l[i] != rhs.l[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (l *List[T]) Clone() *List[T] {
+	ll := &List[T]{l: make([]T, len(l.l))}
+	copy(ll.l, l.l)
+	return ll
+}
+
+func (l *List[T]) Slice() []T {
+	return l.l
+}
+
+func NewList[T comparable](t []T) *List[T] {
+	return &List[T]{l: t}
+}
